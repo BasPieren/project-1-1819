954ace12e292dc8db7419c5e7c11ac58bfdf3d51
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-03-08 11:12:11 +0100

    src/js/modules/filter.js | Edit | Added cover image check

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -4,9 +4,10 @@ function filterData(e) {
         return{
           title: a.titles? a.titles.title._text : 'Geen titel beschikbaar',
           summaries: a.summaries? a.summaries.summary._text : 'Geen omschrijving beschikbaar',
-          cover_image: a.coverimages? a.coverimages.coverimage._text : 'Geen afbeelding beschikbaar'
+          cover_image: a.coverimages.coverimage._text? a.coverimages.coverimage._text : a.coverimages.coverimage[1]._text
         }
       })
+  console.log(allActivities)
   console.log(dataFiltered)
   return dataFiltered
 }
