175015173cb9c23ba192acc95f3dc4bcdc129900
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-03-08 11:11:35 +0100

    src/js/modules/api.js | Edit | Added amount parameter

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -1,12 +1,12 @@
 import { filterData } from './filter.js'
 import { storeData } from './store.js'
 
-async function getData(search, type) {
+async function getData(search, type, amount) {
   const api = new API({
 		key: '1e19898c87464e239192c8bfe422f280'
 	});
 
-  const stream = await api.createStream('search/' + search + '&facet=Type(' + type + '){10}');
+  const stream = await api.createStream('search/' + search + '&facet=Type(' + type + '){' + amount + '}');
 
   stream
     .pipe(filterData)
